117ef57aefbcef012df3a0cebbb176dfb264933f
El_Profesor <sinickis@hotmail.com>
2024-11-03 02:45:38 +0100

    [Dyrektywa] ngFor

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,21 +1,25 @@
 import { Component } from '@angular/core';
+import {Todo} from '../shared/interfaces/todo.interface';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
 export class TodoListComponent {
-  todos : string[] = [];
+  todos : Todo[] = [];
   addTodo(todo : string) : void {
     if (todo.length <= 3){
       alert('Zadanie musi mieć więcej niż 3 znaki');
       return;
     }
 
-    this.todos.push(todo);
+    this.todos.push({name: todo, isCompleted: false});
     console.log('Aktualna lista todo: ', this.todos);
   }
 }
